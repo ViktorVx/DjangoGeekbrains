c0b526f5df0c589b25b95e5718f4bb8f7baf74c0
Viktor Petrov H <viktorptrv@rambler.ru>
2017-08-26 12:11:47 +0300

    Homework 2 is done, but it is not beautiful

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, render_to_response
+from datetime import date
 
 
 
@@ -7,9 +8,12 @@ class Person(object):
 		self.first_name = 'Иван'
 		self.sacond_name = 'Иванович'
 		self.last_name = 'Иванов'
-		self.birthdate = '30 августа 1978 года'
+		self.birthdate = date(1978, 8, 30)
 		self.hobby = 'Люблю программировать на Питоне!'
-
+		#self.studies = ['СОШ № 38 г. Волгограда','Институт изучения внеземных цивилизаций имени Анунака', 'СПБГТФХУ']
+		#self.works = ['ОАО Звездочка','ОАО ПолимерБытПромТехнология', 'ООО ЗверьШкурПром']
+		self.studies = []
+		self.works = []
 
 
 
@@ -18,8 +22,8 @@ def about_my(request):
 	return render_to_response('about_my.html', {'person': Person()})
 
 def study(request):
-	return render_to_response('study.html')
+	return render_to_response('study.html', {'person': Person()})
 
 def work(request):
 	aa = 1
-	return render_to_response('work.html')
+	return render_to_response('work.html', {'person': Person()})
